55e74f5b4d29bd6221b46b4a7494140114bb0806
NhimMaster <thanhhung.nt93@gmail.com>
2025-08-15 17:09:57 +0700

    - fix js masonry

--- wordpress/wp-content/plugins/studio-core/assets/js/custom-masonry.js
+++ wordpress/wp-content/plugins/studio-core/assets/js/custom-masonry.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function ($) {
-    var $grid = $('.custom-masonry-gallery-container');
+    // var $grid = $('.custom-masonry-gallery-container');
 
-    // Khởi tạo Masonry khi các hình ảnh đã được tải xong
-    $grid.imagesLoaded(function () {
-        $grid.masonry({
-            itemSelector: '.custom-masonry-item',
-            columnWidth: '.custom-masonry-item',
-            gutter: 10
-        });
-        // Khởi tạo Lightgallery
-        lightGallery(document.querySelector('.custom-masonry-gallery-container'));
-    });
+    // // Khởi tạo Masonry khi các hình ảnh đã được tải xong
+    // $grid.imagesLoaded(function () {
+    //     $grid.masonry({
+    //         itemSelector: '.custom-masonry-item',
+    //         columnWidth: '.custom-masonry-item',
+    //         gutter: 10
+    //     });
+    //     // Khởi tạo Lightgallery
+    //     lightGallery(document.querySelector('.custom-masonry-gallery-container'));
+    // });
 });
