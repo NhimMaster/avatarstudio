54cb0de2b88c00ceee0b895cfc80015a444e271a
NhimMaster <thanhhung.nt93@gmail.com>
2025-08-16 15:35:28 +0700

    - fix

--- wordpress/wp-content/plugins/studio-core/assets/js/custom-masonry.js
+++ wordpress/wp-content/plugins/studio-core/assets/js/custom-masonry.js
@@ -2,13 +2,13 @@ jQuery(document).ready(function ($) {
     var $grid = $('.custom-masonry-gallery-container');
 
     // Khởi tạo Masonry khi các hình ảnh đã được tải xong
-    $grid.imagesLoaded(function () {
+    // $grid.imagesLoaded(function () {
         $grid.masonry({
             itemSelector: '.custom-masonry-item',
             columnWidth: '.custom-masonry-item',
             gutter: 10
         });
         // Khởi tạo Lightgallery
-        lightGallery(document.querySelector('.custom-masonry-gallery-container'));
-    });
+        // lightGallery(document.querySelector('.custom-masonry-gallery-container'));
+    // });
 });
